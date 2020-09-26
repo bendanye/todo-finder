647e28148be76c8073f2fa091dc5df1e2f8d933f
benjaminng <shen_soonng@hotmail.com>
2020-09-26 16:33:53 +0800

    add test case

--- test/todofinder.test.js
+++ test/todofinder.test.js
@@ -1,7 +1,7 @@
 const { todoFinder } = require('../src/todofinder');
 
 describe('todoFinder', () => {
-  test('should success', () => {
+  test('should success when fully match TODO', () => {
     const records = [
       {
         file: 'test/testfolder/hellotxt.txt',
@@ -16,5 +16,22 @@ describe('todoFinder', () => {
     const result = todoFinder(records);
     expect(result.length).toBe(1);
     expect(result[0].file).toBe('test/testfolder/hellotxt.txt');
+  });
+
+  test('should success when TODO is part of the string', () => {
+    const records = [
+      {
+        file: 'test/testfolder/hellotxt111.txt',
+        data: 'this is testing TODO\r\nthis is one the task to do',
+      },
+      {
+        file: 'test/testfolder/sub1/sub-file1.txt',
+        data: 'this is testing',
+      },
+    ];
+
+    const result = todoFinder(records);
+    expect(result.length).toBe(1);
+    expect(result[0].file).toBe('test/testfolder/hellotxt111.txt');
   });
 });
